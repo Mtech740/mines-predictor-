013c338035541c3d5ca8cd30e50a872e5b31ff96
Mtech740 <mumbasampa3@gmail.com>
2025-04-28 19:21:15 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const button = document.querySelector('button');
+  
+  button.addEventListener('click', function() {
+    alert('This button will soon predict safe tiles!');
+  });
+});
