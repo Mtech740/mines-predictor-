75190118a77363e84e5aa9b14394a5e3e331c5a7
Mtech740 <mumbasampa3@gmail.com>
2025-04-29 10:53:26 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const button = document.querySelector('button');
-  
-  button.addEventListener('click', function() {
-    alert('This button will soon predict safe tiles!');
-  });
-});
