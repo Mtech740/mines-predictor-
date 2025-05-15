73165e67693cc140aff66db772f04a0138523ce4
Mtech740 <mumbasampa3@gmail.com>
2025-05-15 23:33:34 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const grid = document.getElementById("grid");
-grid.innerHTML = "";
-for(let i=0; i<25; i++) {
-  const tile = document.createElement("div");
-  tile.className = "tile";
-  tile.textContent = i+1;
-  grid.appendChild(tile);
-}
